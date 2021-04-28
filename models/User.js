add8542f95993358aefaa5ec04cf5a4a2d8f7f8e
RFeldman84 <rachel.feldman84@gmail.com>
2021-04-27 21:19:48 -0400

    initial commit

--- models/User.js
+++ models/User.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
 
-const catSchema = new Schema({
-  name: String,
-  color: String
-}, {
-  timestamps: {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at'
-  }
-});
+const catSchema = new Schema(
+	{
+		name: String,
+		color: String,
+	},
+	{
+		timestamps: true,
+	}
+)
 
-const Cat = mongoose.model('Cat', catSchema);
-module.exports = Cat;
+const Cat = mongoose.model("Cat", catSchema)
+module.exports = Cat
