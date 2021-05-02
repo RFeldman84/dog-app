d824665c46de9763d4f403635222075c6ec30139
RFeldman84 <rachel.feldman84@gmail.com>
2021-05-02 19:12:37 -0400

    initial set up

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-  .connect(process.env.MONGODB_URI || 'mongodb://localhost/express-template-no-auth', {
+  .connect(process.env.MONGODB_URI || 'mongodb://localhost/dog-app', {
     useCreateIndex: true, 
     useNewUrlParser: true, 
     useUnifiedTopology: true, 
